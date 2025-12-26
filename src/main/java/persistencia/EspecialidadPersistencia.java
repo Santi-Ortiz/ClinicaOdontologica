901a273bd62b08ae2d99b0c28cbdc95974b5bf44
Santi-Ortiz <santi0500@gmail.com>
2025-12-26 13:51:38 -0500

    Implementacion de slf4j para logs

--- src/main/java/persistencia/EspecialidadPersistencia.java
+++ src/main/java/persistencia/EspecialidadPersistencia.java
@@ -2,30 +2,43 @@ package persistencia;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import logica.Especialidad;
 
 public class EspecialidadPersistencia {
 	
 	EspecialidadJpaController especialidadJPAController = new EspecialidadJpaController();
 	
+	public static final Logger logger = LoggerFactory.getLogger(EspecialidadPersistencia.class);
+	
 	public void crearEspecialidad(Especialidad especialidad) {
 		especialidadJPAController.create(especialidad);
+		logger.atDebug().log("La especialidad {}. fue creada", especialidad);
 	}
 	
 	public List<Especialidad> obtenerEspecialidad(){
-		return especialidadJPAController.findEspecialidadEntities();
+		List<Especialidad> especialidades = especialidadJPAController.findEspecialidadEntities();
+		logger.atDebug().log("Se encontraron las especialidades: \n {}.", especialidades);
+		
+		return especialidades;
 	}
 	
 	public Especialidad obtenerEspecialidadPorId(Long id) {
-		return especialidadJPAController.findEspecialidad(id);
+		Especialidad especialidad = especialidadJPAController.findEspecialidad(id);
+		logger.atDebug().log("Se encontro la especialidad: {}.", especialidad);
+		return especialidad;
 	}
 	
-	public void actualizarEspecialidad(Especialidad especialidad) throws Exception  {
+	public void actualizarEspecialidad(Especialidad especialidad) throws Exception {
 		especialidadJPAController.edit(especialidad);
+		logger.atDebug().log("La especialidad {}. fue actualizada", especialidad);
 	}
 	
 	public void eliminarEspecialidad(Long id) throws Exception{
 		especialidadJPAController.destroy(id);
+		logger.atDebug().log("La especialidad con id: {}. fue eliminada", id);
 	}
 
 }
--- src/main/java/persistencia/PersonaPersistencia.java
+++ src/main/java/persistencia/PersonaPersistencia.java
@@ -2,30 +2,46 @@ package persistencia;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import logica.Persona;
 
 public class PersonaPersistencia {
 	
 	PersonaJpaController personaJPAController = new PersonaJpaController();
 	
+	private static Logger logger = LoggerFactory.getLogger(PersonaPersistencia.class);
+	
 	public void crearPersona(Persona persona) {
 		personaJPAController.create(persona);
+//		logger.atDebug().log("La persona {}. fue creada correctamente", persona);
+//		logger.debug("La persona {}. fue creada correctamente", persona);
+		logger.info("La persona {} fue creada correctamente", persona);
 	}
 	
 	public List<Persona> obtenerPersonas(){
-		return personaJPAController.findPersonaEntities();
+		List<Persona> personas = personaJPAController.findPersonaEntities();
+		logger.atDebug().log("Se encontraron las siguientes personas: {}.", personas);
+		
+		return personas;
 	}
 	
 	public Persona obtenerPersonaPorId(Long id){
-		return personaJPAController.findPersona(id);
+		Persona persona = personaJPAController.findPersona(id);
+		logger.atDebug().log("Se encontro la siguiente persona: {}.", persona);
+		
+		return persona;
 	}
 	
 	public void actualizarPersona(Persona persona) throws Exception {
 		personaJPAController.edit(persona);
+		logger.atDebug().log("Se actualizo la persona: {}.", persona);
 	}
 	
 	public void eliminarPersona(Long id) throws Exception{
 		personaJPAController.destroy(id);
+		logger.atDebug().log("Se elimino la persona con id: {}.", id);
 	}
 
 }
